706938737a62f5a4ff1af744333775998c6938b9
Sergey Konnov <sergeyzorro007@gmail.com>
2019-12-09 11:05:29 +0300

    modify script

--- script.pl
+++ script.pl
@@ -7,14 +7,12 @@ use List::Util qw[min max];
 open(WF, "> ./output_fortran_matrix_vector.txt") or die;
 for($i = 10000; $i <= 25000; $i+=1000) {
 	for($j = 1; $j <= 4; $j = $j*2) {
-		$result = 0.0;
-		`mpiifort /Qcoarray:shared /Qcoarray-num-images:$j matrixvector.f90`
+		$result = 100000.0;
+		`mpiifort /Qcoarray:shared /Qcoarray-num-images:$j matrixvector.f90`;
 		for($k = 0; $k < 5; $k++) {
-			$result += `matrixvector`
+			$result = min($result, `matrixvector $i $i` + 0.);
 		}
-		$result/=5
-		print WF $result;
-		print WF " ";
+		print WF $result, " ";
 	}
 	print WF "\n";
 	close(WF);
